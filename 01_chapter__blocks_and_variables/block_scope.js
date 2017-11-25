b909cbb72680dca0e64b34d6a28310228daf055e
Yauheni Svirydzenka <partagas@mail.ru>
2017-11-25 21:43:47 +0300

    1 chapter finished

---
+++ 01_chapter__blocks_and_variables/block_scope.js
@@ -0,0 +1,47 @@
+//var count = 30;
+//let count = 40;
+
+/*const person = {
+    name: "Nicholas"
+};*/
+
+//person.name = "Evgeni";
+//person.age = 30;
+
+//let someVal;
+//console.log(someVal);
+//someVal = 10;
+//console.log(someVal);
+
+//console.log(typeof value);
+//let value = "blue";
+
+/*for (let i = 0; i < 10; i++) {
+    console.log(i);
+}*/
+
+//console.log(i);
+
+
+// -----
+/*let funcs = [];
+
+for (var i = 0; i < 10; i++) {
+    funcs.push((function (value) {
+        return function () {
+            console.log(value);
+        }
+    }(i)));
+}
+
+funcs.forEach(function(func) {
+    func();
+});*/
+
+// -----
+var one = "one";
+let two = "two";
+console.log(one); // one
+console.log(two); // two
+console.log(window.one); // one
+console.log(window.two); // undefined
