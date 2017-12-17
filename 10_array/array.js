8424d538cc62842dc3dfa44e2a1e1f63fa1aa1d0
Yauheni Svirydzenka <partagas@mail.ru>
2017-12-17 14:08:11 +0300

    10 chapter add

---
+++ 10_array/array.js
@@ -0,0 +1,25 @@
+/*let items = new Array(2);
+console.log(items.length);
+console.log(items[0]);
+
+let items2 = new Array("2");
+console.log(items2.length);
+console.log(items2[0]);
+
+let items3 = new Array(1, "2");
+console.log(items3.length);
+console.log(items3[1]);*/
+
+
+// -----
+let items = Array.of(2);
+console.log(items.length);
+console.log(items[0]);
+
+let items2 = Array.of("2");
+console.log(items2.length);
+console.log(items2[0]);
+
+let items3 = Array.of(1, "2");
+console.log(items3.length);
+console.log(items3[1]);
